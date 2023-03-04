21f48c5aaa33fa6afaaf24cdb6a5dad7b9e4c148
felix fedronic <fedronicfelix@gmail.com>
2023-03-05 00:13:39 +0100

    import css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '@/styles/globals.css'
+import '@/styles/flex-slider.css'
+import '@/styles/fontawesome.css'
+import '@/styles/owl.css'
+import '@/styles/templatemo-scholar.css'
+import '@/public/vendor/bootstrap/css/bootstrap.min.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
