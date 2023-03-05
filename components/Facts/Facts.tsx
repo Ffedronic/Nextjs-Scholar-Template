6d60942803b4b4a0ed69e6c757f6a89c9d61e17d
felix fedronic <fedronicfelix@gmail.com>
2023-03-05 17:38:17 +0100

    Facts

--- components/Facts/Facts.tsx
+++ components/Facts/Facts.tsx
@@ -3,41 +3,53 @@ import CountUp from "react-countup";
 
 function Facts() {
   return (
-    <div className="section fun-facts pt-md-3 pb-md-0" id="facts">
+    <div id="facts" className="section">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">
-            <div className="row">
-              <div className="col-lg-3 col-md-6">
-                <div className="counter">
-                  <h3>
-                    <CountUp end={100} />
-                  </h3>
-                  <p className="count-text ">Happy Students</p>
+          <div className="col-lg-12 text-center">
+            <div className="section-heading">
+              <h6>Fun Facts</h6>
+              <h2>Fun Facts</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fun-facts pt-md-3 pb-md-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="row">
+                <div className="col-lg-3 col-md-6">
+                  <div className="counter">
+                    <h3>
+                      <CountUp end={100} />
+                    </h3>
+                    <p className="count-text ">Happy Students</p>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3 col-md-6">
-                <div className="counter">
-                  <h3>
-                    <CountUp end={100} />
-                  </h3>
-                  <p className="count-text ">Course Hours</p>
+                <div className="col-lg-3 col-md-6">
+                  <div className="counter">
+                    <h3>
+                      <CountUp end={100} />
+                    </h3>
+                    <p className="count-text ">Course Hours</p>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3 col-md-6">
-                <div className="counter">
-                  <h3>
-                    <CountUp end={100} />
-                  </h3>
-                  <p className="count-text ">Employed Students</p>
+                <div className="col-lg-3 col-md-6">
+                  <div className="counter">
+                    <h3>
+                      <CountUp end={100} />
+                    </h3>
+                    <p className="count-text ">Employed Students</p>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3 col-md-6">
-                <div className="counter end">
-                  <h3>
-                    <CountUp end={100} />
-                  </h3>
-                  <p className="count-text ">Years Experience</p>
+                <div className="col-lg-3 col-md-6">
+                  <div className="counter end">
+                    <h3>
+                      <CountUp end={100} />
+                    </h3>
+                    <p className="count-text ">Years Experience</p>
+                  </div>
                 </div>
               </div>
             </div>
---
+++ components/Team/Team.tsx
@@ -0,0 +1,119 @@
+import Link from "next/link";
+import React from "react";
+
+function Team() {
+  return (
+    <div className="team section" id="team">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-6">
+            <div className="team-member">
+              <div className="main-content">
+                <img src="assets/images/member-01.jpg" alt="" />
+                <span className="category">UX Teacher</span>
+                <h4>Sophia Rose</h4>
+                <ul className="social-icons">
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-linkedin"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="team-member">
+              <div className="main-content">
+                <img src="assets/images/member-02.jpg" alt="" />
+                <span className="category">Graphic Teacher</span>
+                <h4>Cindy Walker</h4>
+                <ul className="social-icons">
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-linkedin"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="team-member">
+              <div className="main-content">
+                <img src="assets/images/member-03.jpg" alt="" />
+                <span className="category">Full Stack Master</span>
+                <h4>David Hutson</h4>
+                <ul className="social-icons">
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-linkedin"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="team-member">
+              <div className="main-content">
+                <img src="assets/images/member-04.jpg" alt="" />
+                <span className="category">Digital Animator</span>
+                <h4>Stella Blair</h4>
+                <ul className="social-icons">
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#">
+                      <i className="fab fa-linkedin"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
