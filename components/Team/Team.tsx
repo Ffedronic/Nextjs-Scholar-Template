7c3afefbd8197a6667f26fd7630c79262763ccdd
felix fedronic <fedronicfelix@gmail.com>
2023-03-08 21:02:39 +0100

    team title & animation

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -1,118 +1,129 @@
 import Link from "next/link";
 import React from "react";
+import { Fade } from "react-reveal";
 
 function Team() {
   return (
-    <div className="team section" id="team">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3 col-md-6">
-            <div className="team-member">
-              <div className="main-content">
-                <img src="/images/member-01.jpg" alt="member" />
-                <span className="category">UX Teacher</span>
-                <h4>Sophia Rose</h4>
-                <ul className="social-icons">
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-facebook"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-twitter"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-linkedin"></i>
-                    </Link>
-                  </li>
-                </ul>
+    <Fade right>
+      <div className="team section" id="team">
+        <div className="container">
+          <div className="row pb-5">
+            <div className="col-lg-12 text-center">
+              <div className="section-heading">
+                <h6>Team</h6>
+                <h2>Team</h2>
               </div>
             </div>
           </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="team-member">
-              <div className="main-content">
-                <img src="/images/member-02.jpg" alt="member" />
-                <span className="category">Graphic Teacher</span>
-                <h4>Cindy Walker</h4>
-                <ul className="social-icons">
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-facebook"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-twitter"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-linkedin"></i>
-                    </Link>
-                  </li>
-                </ul>
+          <div className="row mt-5">
+            <div className="col-lg-3 col-md-6">
+              <div className="team-member">
+                <div className="main-content">
+                  <img src="/images/member-01.jpg" alt="member" />
+                  <span className="category">UX Teacher</span>
+                  <h4>Sophia Rose</h4>
+                  <ul className="social-icons">
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-facebook"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-twitter"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-linkedin"></i>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="team-member">
-              <div className="main-content">
-                <img src="/images/member-03.jpg" alt="member" />
-                <span className="category">Full Stack Master</span>
-                <h4>David Hutson</h4>
-                <ul className="social-icons">
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-facebook"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-twitter"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-linkedin"></i>
-                    </Link>
-                  </li>
-                </ul>
+            <div className="col-lg-3 col-md-6">
+              <div className="team-member">
+                <div className="main-content">
+                  <img src="/images/member-02.jpg" alt="member" />
+                  <span className="category">Graphic Teacher</span>
+                  <h4>Cindy Walker</h4>
+                  <ul className="social-icons">
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-facebook"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-twitter"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-linkedin"></i>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="team-member">
-              <div className="main-content">
-                <img src="/images/member-04.jpg" alt="member" />
-                <span className="category">Digital Animator</span>
-                <h4>Stella Blair</h4>
-                <ul className="social-icons">
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-facebook"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-twitter"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="fab fa-linkedin"></i>
-                    </Link>
-                  </li>
-                </ul>
+            <div className="col-lg-3 col-md-6">
+              <div className="team-member">
+                <div className="main-content">
+                  <img src="/images/member-03.jpg" alt="member" />
+                  <span className="category">Full Stack Master</span>
+                  <h4>David Hutson</h4>
+                  <ul className="social-icons">
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-facebook"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-twitter"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-linkedin"></i>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div className="team-member">
+                <div className="main-content">
+                  <img src="/images/member-04.jpg" alt="member" />
+                  <span className="category">Digital Animator</span>
+                  <h4>Stella Blair</h4>
+                  <ul className="social-icons">
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-facebook"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-twitter"></i>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="#">
+                        <i className="fab fa-linkedin"></i>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 }
 
