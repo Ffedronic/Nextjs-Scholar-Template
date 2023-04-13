ed74a5f483c09a2b246ed94897a96b83b48b8010
felix fedronic <fedronicfelix@gmail.com>
2023-04-13 22:23:07 +0200

    final commit

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,9 +4,11 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Preloader from "../Preloader/Preloader";
 import $ from "jquery";
+
 interface LayoutProps {
   children: ReactNode;
 }
+
 function Layout(props: LayoutProps) {
   const { children } = props;
 
